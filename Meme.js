5ddb31b0935f44246e32e6f00fdaba8977447d36
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-08 13:35:01 -0400

    Update CSS and proofread some HTML outlined JS for next steps

--- Meme.js
+++ Meme.js
@@ -1,43 +1,50 @@
 const picinput = document.querySelector("#pic");
-const textinput = document.querySelector("#textInput")
+const textinput = document.querySelector("#textInput");
+const preview = document.querySelector("#memeinput");
 const meme = document.querySelector('#meme');
 //HTML to be inserted as generator works
 const topinput = "<class='form-inline' input='text' placeholder='Top Text' name='topText'>"
 const bottominput = "<class='form-inline' input='text' placeholder='Bottom Text' name='bottomText'>"
-const picdisplay = "<img = "+picinput.value+" alt='Your Meme'>"
+const picdisplay = "<img = "+picinput.value+" class='meme' alt='Your Meme'>"
 // use form action to run TextboxLocation function
     // **Done** TextboxLocation reads id of selected
-    // Puts Gray box where text to be located
+    // ?? Puts Gray box where text to be located  
     // **Done** creates 1 or 2 forms for text (top 1/bottom 1/both 2) 
 function TextboxLocation(){
     const location = document.getElementById('text').value
     if (location == "Top"){ //should create a singular text input form placeholder Top Text
         const toptext = textinput.createElement("form")
         toptext.innerHTML = topinput
-        
+        //need to append child
         return;
     }
     if (location == "Bottom"){//should create a singular text input form placeholder Bottom Text
         const bottomtext = textinput.createElement("form")
         bottomtext.innerHTML = bottominput
         return;
+        //need to append child
     }
     if (location == "Both"){ //should create both input forms for text
         const toptext = textinput.createElement("form")
         toptext.innerHTML = topinput
         const bottomtext = textinput.createElement("form")
         bottomtext.innerHTML = bottominput
+        //need to append children
         return;
     }
 }
 
-function previewPic(){
+function previewPic(){ //creates preview thumbnail of image
     picinput = document.querySelector("#pic").value
-    const pic = meme.createElement("img")
+    const pic = preview.createElement("img")
     pic.innerHTML = picdisplay
 }
 
-function generatememe(){
+function generatememe(e){ //puts everything together to create Meme
+    e.preventDefault()
+
+    //needs values for Text Location and Image at minimum
+    //on Submit - Place IMG, place textboxes, place text.
 
 }
 
