3232aabc40d8dd3d5cc01284dfd89fa0e5adcff9
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-09 07:52:54 -0400

    45th attempt at reworking.  JSON add

--- Meme.js
+++ Meme.js
@@ -2,49 +2,51 @@ const picinput = document.querySelector("#pic");
 const textinput = document.querySelector("#textInput");
 const preview = document.querySelector("#memeinput");
 const meme = document.querySelector('#meme');
-//HTML to be inserted as generator works
-const topinput = "<class='form-inline' input='text' placeholder='Top Text' name='topText'>"
-const bottominput = "<class='form-inline' input='text' placeholder='Bottom Text' name='bottomText'>"
-const picdisplay = "<img = "+picinput.value+" class='meme' alt='Your Meme'>"
+
+const picdisplay = "<img src="+ picinput.value +" class='meme' alt='Your Meme'>"
 // use form action to run TextboxLocation function
     // **Done** TextboxLocation reads id of selected
     // ?? Puts Gray box where text to be located  
-    // **Done** creates 1 or 2 forms for text (top 1/bottom 1/both 2) 
+const toptext = document.querySelector("#top")
+const bottomtext = document.querySelector("#bottom")
+
+meme.addEventListener("load",function(){
+    JSON.parse(localStorage('picURL','imgHTML'))
+    meme.innerHTML=imgHTML
+})
 function TextboxLocation(){
-    const location = document.getElementById('text').value
-    if (location == "Top"){ //should create a singular text input form placeholder Top Text
-        const toptext = textinput.createElement("form")
-        toptext.innerHTML = topinput
-        //need to append child
+    if(bottomtext.value === "" ){ //outputs top text only
+        console.log(toptext.value)
+        
+        //maketop(toptext)
         return;
-    }
-    if (location == "Bottom"){//should create a singular text input form placeholder Bottom Text
-        const bottomtext = textinput.createElement("form")
-        bottomtext.innerHTML = bottominput
+    }else if( toptext.value != "" && bottomtext.value!=""){//outputs both lines
+        console.log(toptext.value)
+        console.log(bottomtext.value)
+        //maketop(toptext)
+        //makebottom(bottomtext)
         return;
-        //need to append child
-    }
-    if (location == "Both"){ //should create both input forms for text
-        const toptext = textinput.createElement("form")
-        toptext.innerHTML = topinput
-        const bottomtext = textinput.createElement("form")
-        bottomtext.innerHTML = bottominput
-        //need to append children
+    }else
+        console.log(bottomtext.value) //outputs bottom line
+        //makebottom(bottomtext)
         return;
-    }
+}
+// picinput.addEventListener("submit",placepicture)
+function placepicture(p){
+    p.preventDefault()
+    localStorage.setItem("picURL",JSON.stringify(picinput.value))
+    localStorage.setItem("imgHTML",JSON.stringify(picdisplay))
+    console.log(picinput.value, picdisplay)
+    // const pic = document.createElement("img")
+    // pic.innerHTML = picdisplay
+    // meme.appendChild(pic);
+    // meme.className(meme)
 }
 
-function previewPic(){ //creates preview thumbnail of image
-    picinput = document.querySelector("#pic").value
-    const pic = preview.createElement("img")
-    pic.innerHTML = picdisplay
-}
+function Creatememe(){ //puts everything together to create Meme
+    TextboxLocation
+    placepicture
 
-function generatememe(e){ //puts everything together to create Meme
-    e.preventDefault()
-
-    //needs values for Text Location and Image at minimum
-    //on Submit - Place IMG, place textboxes, place text.
 
 }
 
