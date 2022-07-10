7b813dff667e8eb6c299c77af23ac356ddf8930a
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-10 11:50:03 -0400

    attempting Canvas for dynamic text

--- Meme.js
+++ Meme.js
@@ -2,13 +2,17 @@ const picinput = document.querySelector("#pic");
 const textinput = document.querySelector("#textInput");
 const preview = document.querySelector("#memeinput");
 const meme = document.querySelector('#meme');
+const clearmeme = document.querySelector("#clearmeme")
+const form = document.querySelector('form')
+form.addEventListener("submit",placepicture)
 
-const picdisplay = "<img src="+ picinput.value +" class='meme' alt='Your Meme'>"
 // use form action to run TextboxLocation function
     // **Done** TextboxLocation reads id of selected
     // ?? Puts Gray box where text to be located  
 const toptext = document.querySelector("#top")
+toptext.addEventListener("keyup",topType)
 const bottomtext = document.querySelector("#bottom")
+bottomtext.addEventListener("Change",bottomType)
 
 window.addEventListener("load",function(){
     let memeinnerHTML = this.localStorage.getItem('imgHTML')
@@ -18,6 +22,16 @@ window.addEventListener("load",function(){
 
     
 })
+function topType(){
+    console.log(this.value)
+    bannertop=this.value;
+    ctx.fillRect(0,0,meme.width,meme.height)
+    ctx.fillText(bannertop,50,50)
+}
+function bottomType(){
+
+}
+
 function TextboxLocation(){
     if(bottomtext.value === "" ){ //outputs top text only
         console.log(toptext.value)
@@ -35,16 +49,13 @@ function TextboxLocation(){
         //makebottom(bottomtext)
         return;
 }
-// picinput.addEventListener("submit",placepicture)
+
 function placepicture(p){
     p.preventDefault()
-    localStorage.setItem("picURL",JSON.stringify(picinput.value))
-    localStorage.setItem("imgHTML",JSON.stringify(picdisplay))
-    console.log(picinput.value, picdisplay)
-    // const pic = document.createElement("img")
-    // pic.innerHTML = picdisplay
-    // meme.appendChild(pic);
-    // meme.className(meme)
+    let ctx = meme.getContext('2d')
+    clearmeme.setAttribute('width',newImage.width)
+    clearmeme.setAttribute('height',newImage.height)
+    ctx.drawImage(Image,10,10);
 }
 
 function Creatememe(){ //puts everything together to create Meme
