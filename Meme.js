656fce34e78202da57adc09f2e4d83b851f2341d
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-17 19:48:49 -0400

    modules for javascript code and some HTML updates

--- Meme.js
+++ Meme.js
@@ -1,46 +1,66 @@
 const picinput = document.querySelector("#pic");
 
-const meme = document.querySelector("#meme");
+
+window.addEventListener('DOMContentLoaded',function(){
+    createdivbox()
+    })
+
+
 const form = document.querySelector('form')
 form.addEventListener("submit",placepicture)
-
-const toptext = document.querySelector("#top")
-toptext.addEventListener("keyup",topType)
-const bannertop = document.querySelector("#toptext")
-const bottomtext = document.querySelector("#bottom")
-bottomtext.addEventListener("keyup",bottomType)
-const bannerbottom = document.querySelector("#bottomtext")
 
 const textcolor = document.querySelector("#textcolor")
 textcolor.addEventListener("input",changeTxtColor)
 
 
 function changeTxtColor(){
-bannertop.style.color = this.value
-bannerbottom.style.color = this.value
+document.querySelector("#toptext").style.color = this.value
+document.querySelector("#bottomtext").style.color = this.value
 return;
 }
-
+const toptext = document.querySelector("#top")
+toptext.addEventListener("keyup",topType)
 function topType(){
+    const bannertop = document.querySelector("#toptext")
     bannertop.innerHTML=this.value;
 }
-
+const bottomtext = document.querySelector("#bottom")
+bottomtext.addEventListener("keyup",bottomType)
 function bottomType(){
+    const bannerbottom = document.querySelector("#bottomtext")
     bannerbottom.innerHTML=this.value;
 }
 
-function placepicture(p){
+ function placepicture(p){
     p.preventDefault()
-//    createdivbox
-    const urlString = "url ("+picinput.value+");"
-    meme.style="background-image: "+urlString
-    meme.setAttribute("width","400px");
-    meme.setAttribute("height","400px");
-    console.log(meme.clientWidth)
+    createdivbox
+    const bkgdstring = 'background-image: url('+picinput.value+');'
+    meme.style.cssText += bkgdstring+'height: 400px;width:450px;id: meme;class: meme'
 }
+
 function createdivbox(){
      const newdiv = document.createElement("div")
-     meme.append(newdiv)
+     const body = document.querySelector("body")
      newdiv.setAttribute("class","meme")
      newdiv.setAttribute("id","meme")
+
+     body.append(newdiv)
+     return;
+}
+function newtopdiv(newdiv){
+    const newtop = document.createElement("div");
+    newtop.setAttribute("class","toptext")
+    newtop.setAttribute("id","toptext")
+    newtop.setAttribute("style","color:white")
+    newdiv.append(newtop)
+    return newtop;
+}
+
+function newbottomdiv(newdiv){
+    const newbottom = document.createElement("div");
+    newbottom.setAttribute("class","bottomtext")
+    newbottom.setAttribute("id","bottomtext")
+    newbottom.setAttribute("style","color:white")
+    newdiv.append(newbottom)
+    return newbottom;
 }
