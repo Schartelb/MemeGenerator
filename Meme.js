978108415d2474d58945a5f05322987156ff0e85
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-17 10:31:56 -0400

    Further attempts at Div box editing

--- Meme.js
+++ Meme.js
@@ -1,7 +1,6 @@
 const picinput = document.querySelector("#pic");
-const textinput = document.querySelector("#textInput");
-const meme = document.querySelector("img");
-//const clearmeme =document.querySelector('#clearmeme');
+
+const meme = document.querySelector("#meme");
 const form = document.querySelector('form')
 form.addEventListener("submit",placepicture)
 
@@ -15,12 +14,7 @@ const bannerbottom = document.querySelector("#bottomtext")
 const textcolor = document.querySelector("#textcolor")
 textcolor.addEventListener("input",changeTxtColor)
 
-//window.addEventListener("load",function(){
- //   let memeinnerHTML = this.localStorage.getItem('imgHTML')
-//    meme.innerHTML=memeinnerHTML
-//    let picURL = this.localStorage.getItem('picURL')
-//    picinput.value = picURL
-//})
+
 function changeTxtColor(){
 bannertop.style.color = this.value
 bannerbottom.style.color = this.value
@@ -28,60 +22,25 @@ return;
 }
 
 function topType(){
-    changeTxtColor
-    // console.log(this.value)
     bannertop.innerHTML=this.value;
-    //bannertop.setAttribute("font-size","calc(.5 * "+meme.width+" / "+bannertop.innerHTML.length+"")
-    
-    // ctx.fillRect(0,0,meme.width,meme.height)
-    // ctx.fillText(bannertop,50,50)
 }
+
 function bottomType(){
-    changeTxtColor
-    // console.log(this.value)
     bannerbottom.innerHTML=this.value;
-    //bannerbottom.setAttribute("font-size","calc(.5 * "+meme.width+" / "+bannerbottom.innerHTML.length+"")
-    
-    // ctx.fillRect(0,0,meme.width,meme.height)
-    // ctx.fillText(bannerbottom,50,50)
 }
-//Artifact from initial attempts at textbox locating
-// function TextboxLocation(){
-//     if(bottomtext.value === "" ){ //outputs top text only
-//         console.log(toptext.value)
-        
-//         //maketop(toptext)
-//         return;
-//     }else if( toptext.value != "" && bottomtext.value!=""){//outputs both lines
-//         console.log(toptext.value)
-//         console.log(bottomtext.value)
-//         //maketop(toptext)
-//         //makebottom(bottomtext)
-//         return;
-//     }else
-//         console.log(bottomtext.value) //outputs bottom line
-//         //makebottom(bottomtext)
-//         return;
-// }
 
 function placepicture(p){
     p.preventDefault()
-    meme.src=picinput.value
+//    createdivbox
+    const urlString = "url ("+picinput.value+");"
+    meme.style="background-image: "+urlString
+    meme.setAttribute("width","400px");
+    meme.setAttribute("height","400px");
     console.log(meme.clientWidth)
-//Artifact from Canvas Attempt
-// clearmeme.setAttribute("width",clearmeme.width)
-// clearmeme.setAttribute("height",clearmeme.height)
-//    let ctx = meme.getContext('2d')
-//    ctx.drawImage(Image,10,10);
 }
-
-//Artifact from separate Image/Text submits
-// function Creatememe(){ //puts everything together to create Meme
-//     TextboxLocation
-//     placepicture
-// }
-
-
-
-
-
+function createdivbox(){
+     const newdiv = document.createElement("div")
+     meme.append(newdiv)
+     newdiv.setAttribute("class","meme")
+     newdiv.setAttribute("id","meme")
+}
